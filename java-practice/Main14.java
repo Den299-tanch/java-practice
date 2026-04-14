cf8d55922d759935dedf45658f84815b0d67e166
Den299-tanch <harunarun555@gmail.com>
2026-04-15 04:28:19 +0900

    Add files via upload

---
+++ java-practice/Main14.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class Main14 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            String name = sc.next();
+            int old = sc.nextInt();
+            String birth = sc.next();
+            String area = sc.next();
+            
+            System.out.println("User{");
+            System.out.println("nickname : " + name);
+            System.out.println("old : " + old);
+            System.out.println("birth : " + birth);
+            System.out.println("state : " + area);
+            System.out.println("}");
+            
+        }
+    }
+}
+
